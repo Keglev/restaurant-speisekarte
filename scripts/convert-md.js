79faafba6c7ebed9e33ee9ca0e78e27f5ca6a07b
Keglev <ckbuzin1@gmail.com>
2025-10-22 16:31:29 +0200

    docs: change deployment jsdoc destination; add markdown converter; build scripts

---
+++ scripts/convert-md.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { marked } = require('marked');
+
+if (process.argv.length < 4) {
+  console.error('Usage: node scripts/convert-md.js <srcDir> <outDir>');
+  process.exit(1);
+}
+
+const srcDir = process.argv[2];
+const outDir = process.argv[3];
+
+function ensureDir(dir) {
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+}
+
+function convertFile(srcPath, outPath) {
+  const md = fs.readFileSync(srcPath, 'utf8');
+  const html = marked(md);
+  const outHtml = `<!doctype html>\n<html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>${path.basename(srcPath)}</title></head><body>${html}</body></html>`;
+  fs.writeFileSync(outPath, outHtml, 'utf8');
+}
+
+function walkAndConvert(srcRoot, outRoot) {
+  ensureDir(outRoot);
+  const entries = fs.readdirSync(srcRoot, { withFileTypes: true });
+  for (const entry of entries) {
+    const srcPath = path.join(srcRoot, entry.name);
+    const outPath = path.join(outRoot, entry.name.replace(/\.md$/i, '.html'));
+    if (entry.isDirectory()) {
+      walkAndConvert(srcPath, path.join(outRoot, entry.name));
+    } else if (/\.md$/i.test(entry.name)) {
+      ensureDir(path.dirname(outPath));
+      convertFile(srcPath, outPath);
+      console.log('Converted', srcPath, '->', outPath);
+    }
+  }
+}
+
+walkAndConvert(srcDir, outDir);
